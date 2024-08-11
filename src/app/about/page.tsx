41d676f536b126591ba236958fd66e119a8dc43d
SHiuN26 <justodme26@gmail.com>
2024-08-12 02:10:50 +0800

    afaf

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./title";
+
+const About = () => {
+  return (
+    <>
+      <Title />
+    </>
+  );
+};
+
+export default About;
---
+++ src/app/about/title.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Title = () => {
+  return <div>About - Title</div>;
+};
+
+export default Title;
